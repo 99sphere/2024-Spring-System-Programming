c3ffbd77709d6156ae79bad3fb1b3bb42c700138
이구 <99sphere@gmail.com>
2024-06-02 21:39:27 +0900

    =

--- client/main.c
+++ client/main.c
@@ -16,6 +16,7 @@
 pthread_mutex_t map_mutex;
 pthread_mutex_t qr_mutex;
 
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         printf("Usage: %s <Server IP> <port number> <cur dir>\n", argv[0]);
@@ -92,14 +93,51 @@ int main(int argc, char* argv[]) {
         printf("Error: unable to create thread, %d\n", map_thread_ret);
         exit(-1);
     }
+                       상     우     하      좌
+    int dir[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};
+    
+    0,0
+
+    int prev_x;
+    int prev_y;
 
     while(1){
         // 서버에서 데이터를 받아왔나?
+        printf("[Main Loop] cur_x: %d, cur_y: %d, cur_status: %d", cur_x, cur_y, raw_map.map[cur_x][cur_y].item.status);
+        // cur_x
+        // cur_y
+        // for(int d = 0;d<4;d++){
+        //     int ny = y+dir[d][0];
+        //     int nx = x+dir[d][1];
+            
+        //     if(( ny >= 0 && nx >= 0) && (ny < 5 && nx < 5)){
+        //         raw_map.map[nx][ny].item.status
+        //         if(raw_map.map[nx][ny]){
 
-        // cur_x, cur_y -> 현재 위치
+        //         }
+        //     }
+        // }
         printf("[Main Algorithm]\n");
         printMap(raw_map);
         sleep(1);
+
+
+
+        /*
+        
+        switch (dgist.map[i][j].item.status) {
+            case nothing:
+                printf("- ");
+                break;
+            case item:
+                printf("%d ", dgist.map[i][j].item.score);
+                break;
+            case trap:
+                printf("T ");
+                break;
+        }
+            
+            */
     }
     // 스레드가 종료될 때까지 대기
     pthread_join(thread_qr, NULL);
