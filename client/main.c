c980301421252ecb712f0781f6620316464a011c
이구 <99sphere@gmail.com>
2024-06-02 17:51:04 +0900

    =

--- client/main.c
+++ client/main.c
@@ -91,7 +91,7 @@ int main(int argc, char* argv[]) {
 
     while(1){
         printf("[Main Algorithm]\n");
-        printMap(map_thread_data.raw_map_ptr);
+        printMap(raw_map);
         sleep(1);
     }
     // 스레드가 종료될 때까지 대기
