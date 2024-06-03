af279ac92ee325fe83f8df1161108257d6cec434
이구 <99sphere@gmail.com>
2024-06-03 15:34:24 +0900

    =

--- client/control.h
+++ client/control.h
@@ -4,8 +4,6 @@
 #define PIN_R2 7
 #define DEVICE_ADDR 0x16
 
-int fd;
-
 int write_i2c_block_data(int reg, unsigned char* data, int length);
 int ctrl_car(int l_dir, int l_speed, int r_dir, int r_speed);
 int control_servo(int id, int angle);
